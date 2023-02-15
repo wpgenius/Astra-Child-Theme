b66ffaad1f302446e438d920eef5ac153207df41
sohamjoshi827 <soham@wpgenius.in>
2023-02-15 16:58:39 +0530

    Enqueue child theme stylesheet.

---
+++ includes/theme-actions.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * Add actions related to Theme.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package astra-child-theme
+ */
+
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 'astra-theme-css', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->parent()->get( 'Version' ) );
+	wp_enqueue_style( 'astra-child-theme', get_stylesheet_uri(), array( 'astra-theme-css' ), wp_get_theme()->get( 'Version' )	);
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
