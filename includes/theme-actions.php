da6a061ffcbad1cc3fa9e55d3bde1913f6814c18
sohamjoshi827 <soham@wpgenius.in>
2023-02-17 19:12:31 +0530

    Disable Gutenberg editor.

--- includes/theme-actions.php
+++ includes/theme-actions.php
@@ -8,6 +8,25 @@
 
 function my_theme_enqueue_styles() {
 	wp_enqueue_style( 'astra-theme-css', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->parent()->get( 'Version' ) );
-	wp_enqueue_style( 'astra-child-theme', get_stylesheet_uri(), array( 'astra-theme-css' ), wp_get_theme()->get( 'Version' )	);
+	wp_enqueue_style( 'astra-child-theme', get_stylesheet_uri(), array( 'astra-theme-css' ), wp_get_theme()->get( 'Version' ) );
+
+	// Remove Gutenberg Block Library CSS from loading on the frontend
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'bp-member-block' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'wc-block-style' );
+	wp_dequeue_style( 'wc-blocks-vendors-style' );
+	wp_deregister_style( 'wc-block-editor' );
+	wp_deregister_style( 'wc-blocks-style' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+/**
+ * Disable Gutenberg on the back end.
+ */
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+/**
+ * Disable Gutenberg for widgets.
+ */
+add_filter( 'use_widgets_block_editor', '__return_false' );
