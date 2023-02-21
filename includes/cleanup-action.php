d2a6ce8e59846877a300ecdef43c7cef33b9595c
sohamjoshi827 <soham@wpgenius.in>
2023-02-21 12:51:48 +0530

    Some fixes and commenting in cleanup-action.php

--- includes/cleanup-action.php
+++ includes/cleanup-action.php
@@ -14,33 +14,37 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'WPGenius_cleanup_action' ) ) {
 	class WPGenius_cleanup_action {
 		public static $instance;
-	
+
 		public static function init() {
 			if ( is_null( self::$instance ) ) {
 				self::$instance = new WPGenius_cleanup_action();
 			}
 			return self::$instance;
 		}
-	
+
 		private function __construct() {
-	
-			add_action( 'wp_enqueue_scripts', array( $this, 'enque_scripts' ) );
+
+			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 			/**
 			 * Disable Gutenberg on the back end.
 			 */
-			add_filter( 'use_block_editor_for_post', array( $this, '__return_false' ) );
-	
+			add_filter( 'use_block_editor_for_post', '__return_false' );
+
 			/**
 			 * Disable Gutenberg for widgets.
 			 */
-			add_filter( 'use_widgets_block_editor', array( $this, '__return_false' ) );
-	
-			add_action( 'wp_dashboard_setup', array( $this, 'remove_unwanted_widgets', 9999 ) );
-	
+			add_filter( 'use_widgets_block_editor', '__return_false' );
+
+			add_action( 'wp_dashboard_setup', array( $this, 'remove_unwanted_widgets' ), 9999 );
+
 		}
-	
-		public function enque_scripts() {
-			// Remove Gutenberg Block Library CSS from loading on the frontend
+
+		/**
+		 * Remove Gutenberg Block Library CSS from loading on the frontend
+		 *
+		 * @return void
+		 */
+		public function enqueue_scripts() {
 			wp_dequeue_style( 'wp-block-library' );
 			wp_dequeue_style( 'bp-member-block' );
 			wp_dequeue_style( 'wp-block-library-theme' );
@@ -49,12 +53,11 @@ if ( ! class_exists( 'WPGenius_cleanup_action' ) ) {
 			wp_deregister_style( 'wc-block-editor' );
 			wp_deregister_style( 'wc-blocks-style' );
 		}
-	
-	
+
 		/**
 		 * Remove unwanted widgets from dashboard
 		 *
-		 * @return array
+		 * @return void
 		 */
 		public function remove_unwanted_widgets() {
 			global $wp_meta_boxes;
