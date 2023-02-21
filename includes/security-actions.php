020f42ecf9e122270c60c9d48bb97a764038c809
Makarand Mane <mane.makarand@gmail.com>
2023-02-21 19:02:45 +0530

    Disable XMLRPC

--- includes/security-actions.php
+++ includes/security-actions.php
@@ -43,6 +43,11 @@ if ( ! class_exists( 'WPGenius_security_action' ) ) {
 			 * Remove WordPress Meta Generator Tag
 			 */
 			remove_action('wp_head', 'wp_generator');
+			
+			/**
+			 * Disable XMLRPC
+			 */
+			add_filter('xmlrpc_enabled', '__return_false');
 		}
 	}
 	WPGenius_security_action::init();
