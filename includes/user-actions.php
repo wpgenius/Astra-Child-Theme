9f6de1bceb8ea2803caab9dd75059e047830ca21
Makarand Mane <mane.makarand@gmail.com>
2023-02-24 16:38:40 +0530

    Fix #1 Remove Login Shake Animation and Hide Login Errors in WordPress on production environment

--- includes/user-actions.php
+++ includes/user-actions.php
@@ -44,7 +44,37 @@ if ( ! class_exists( 'WPGenius_user_actions' ) ) {
 			 * Remove application password settings from user profile
 			 */
 			add_filter( 'wp_is_application_passwords_available', '__return_false' );
+
+			/**
+			 * Remove Login Shake Animation
+			 */
+			if( WP_ENVIRONMENT_TYPE === 'production' ){
+				add_action( 'login_footer', array( $this, 'remove_shake_js' ) );
+				add_filter( 'login_errors', array( $this, 'hide_login_error' ) );
+			}
+
 		}
+
+		/**
+		 * Remove Login Shake Animation
+		 *
+		 * @return void
+		 */
+		public function remove_shake_js() {
+			remove_action( 'login_footer', 'wp_shake_js', 12 );
+		}
+
+		/**
+		 * Hide Login Errors in WordPress. Add custom login error message
+		 *
+		 * @param string $error
+		 * @return string
+		 */
+		public function hide_login_error( $error ) {
+			// Edit the line below to customize the message.
+			return __( 'Something is wrong!', 'astra-child-theme' );
+		}
+
 	}
 	WPGenius_user_actions::init();
 	
