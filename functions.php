a93b75d730f8858334f7df4245dba8030567a25e
Makarand Mane <mane.makarand@gmail.com>
2023-02-24 14:05:46 +0530

    ref #7 Allow SVG uploads for administrator users.

--- functions.php
+++ functions.php
@@ -7,4 +7,10 @@
  * @package astra-child-theme
  */
 
+/**
+ * Allow administrator to upload SVG file.
+ * Default : false
+ */
+define( 'ALLOW_SVG', false );
+
 require get_stylesheet_directory() . '/includes/theme-functions.php';
