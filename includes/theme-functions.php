b81b4867b0dbb569b0e58354e9d5cc0387c40f3e
Makarand Mane <mane.makarand@gmail.com>
2023-03-16 19:26:08 +0530

    Fix #4 restructure files

--- includes/theme-functions.php
+++ includes/theme-functions.php
@@ -9,15 +9,15 @@
 
 require get_stylesheet_directory() . '/includes/theme-actions.php';
 require get_stylesheet_directory() . '/includes/theme-settings.php';
-require get_stylesheet_directory() . '/includes/admin-actions.php';
 require get_stylesheet_directory() . '/includes/user-actions.php';
-require get_stylesheet_directory() . '/includes/seo-actions.php';
 require get_stylesheet_directory() . '/includes/ajax-actions.php';
 require get_stylesheet_directory() . '/includes/woo-actions.php';
 require get_stylesheet_directory() . '/includes/theme-shortcodes.php';
 if ( did_action( 'elementor/loaded' ) ) {
 	require get_stylesheet_directory() . '/includes/widgets-elementor.php';
 }
+require get_stylesheet_directory() . '/includes/modules/seo-actions.php';
+require get_stylesheet_directory() . '/includes/modules/admin-actions.php';
 require get_stylesheet_directory() . '/includes/modules/security-actions.php';
 require get_stylesheet_directory() . '/includes/modules/cleanup-action.php';
 require get_stylesheet_directory() . '/includes/modules/theme-configurator.php';
