7b0977a10fb6648f6597787db957e2243002d6c4
Makarand Mane <mane.makarand@gmail.com>
2023-03-17 16:33:27 +0530

    Add a file to Add Functions related to themes.

---
+++ includes/theme-functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Add common useful functions related to themes here.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package astra-child-theme
+ */
+
+ 
