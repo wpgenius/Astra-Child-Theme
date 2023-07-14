992cba6b5b89e7df58424d5c975b2191f5749f75
sohamjoshi827 <soham@wpgenius.in>
2023-07-14 15:09:06 +0530

    1. Rename text of VAT to GST on cart page. 2. Rename VAT to GST in Woocommerce emails and checkout page.

--- includes/woo-actions.php
+++ includes/woo-actions.php
@@ -2,7 +2,7 @@
 /**
  * Woocommerce actions from child theme
  *
- * @package astra-child-theme
+ * @package forestsecrets
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -39,7 +39,36 @@ if ( ! class_exists( 'WPGenius_woo_actions' ) && class_exists( 'WooCommerce' ) )
 		 * Class constructor
 		 */
 		private function __construct() {
+			add_filter( 'woocommerce_countries_inc_tax_or_vat', array( 'cart_totals_order_total_html' ), 10, 1 );
+			add_filter( 'woocommerce_get_formatted_order_total', array( 'cart_totals_order_total_html' ), 10, 1 );
+			add_filter( 'gettext', array( $this, 'woo_email' ), 10, 3 );
 			// Add hooks here
+		}
+
+		/**
+		 * Rename text of VAT to GST on cart page.
+		 *
+		 * @param array $value
+		 * @return void
+		 */
+		function cart_totals_order_total_html( $value ) {
+			return preg_replace( '/\(includes[^)]+\)/', '(inclusive GST)', $value );
+		}
+
+		/**
+		 * Rename VAT to GST in Woocommerce emails and checkout page
+		 *
+		 * @param array $translation
+		 * @param array $text
+		 * @param array $domain
+		 * @return void
+		 */
+		function woo_email( $translation, $text, $domain ) {
+			if ( $domain == 'woocommerce' ) {
+				if ( $text == 'VAT' ) {
+					$translation = 'GST'; }
+			}
+			return $translation;
 		}
 	}
 	WPGenius_woo_actions::init();
