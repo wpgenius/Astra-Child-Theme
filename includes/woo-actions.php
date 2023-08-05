1ce5b074351b6cb232814fb81fcf2299d758001a
sohamjoshi827 <soham@wpgenius.in>
2023-08-05 19:10:26 +0530

    Hide shipping rates when free shipping is available.

--- includes/woo-actions.php
+++ includes/woo-actions.php
@@ -40,6 +40,7 @@ if ( ! class_exists( 'WPGenius_woo_actions' ) && class_exists( 'WooCommerce' ) )
 		 */
 		private function __construct() {
 			add_filter( 'gettext', array( $this, 'woo_email' ), 10, 3 );
+			add_filter( 'woocommerce_package_rates', array( $this, 'hide_shipping_when_free_is_available' ), 100 );
 			// Add hooks here
 		}
 
@@ -57,6 +58,24 @@ if ( ! class_exists( 'WPGenius_woo_actions' ) && class_exists( 'WooCommerce' ) )
 					$translation = 'GST'; }
 			}
 			return $translation;
+		}
+
+		/**
+		 * Hide shipping rates when free shipping is available.
+		 * Updated to support WooCommerce 2.6 Shipping Zones.
+		 *
+		 * @param array $rates Array of rates found for the package.
+		 * @return array
+		 */
+		function hide_shipping_when_free_is_available( $rates ) {
+			$free = array();
+			foreach ( $rates as $rate_id => $rate ) {
+				if ( 'free_shipping' === $rate->method_id ) {
+					$free[ $rate_id ] = $rate;
+					break;
+				}
+			}
+			return ! empty( $free ) ? $free : $rates;
 		}
 	}
 	WPGenius_woo_actions::init();
