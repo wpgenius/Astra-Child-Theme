a936f72b2f1590d18c6e656dd361615d810c9c40
sohamjoshi827 <soham@wpgenius.in>
2024-03-18 19:04:11 +0530

    Revert "Fix: Resolved the bug Disable gutenberg block editor when elementor is active."
    This reverts commit 065e19ac57c0126339cb7118949340224f87bcf8.

--- includes/modules/admin-actions.php
+++ includes/modules/admin-actions.php
@@ -45,7 +45,21 @@ if ( ! class_exists( 'WPGenius_admin_actions' ) ) {
 			 */
 			add_filter( 'wp_is_application_passwords_available', '__return_false' );
 
+			if (is_plugin_active( 'elementor/elementor.php' )) {
 
+				/**
+				 * Disable Gutenberg on the back end.
+				 */
+				add_filter( 'use_block_editor_for_post', '__return_false', 5 );
+
+				add_filter( 'gutenberg_can_edit_post', '__return_false', 5 );
+
+				/**
+				* Disable Gutenberg for widgets.
+				*/
+				add_filter( 'use_widgets_block_editor', '__return_false' );
+
+			}
 
 			/**
 			 * Allow SVG uploads
@@ -100,29 +114,6 @@ if ( ! class_exists( 'WPGenius_admin_actions' ) ) {
 				define( 'UAEL_WL_INTERNAL_HELP_LINKS', 'disable' );
 			}
 
-		}
-
-		/**
-		 * Disable gutenberg block editor when elementor is active.
-		 *
-		 * @return void
-		 */
-		function disable_gutenberg() {
-			if (is_plugin_active( 'elementor/elementor.php' )) {
-
-				/**
-				 * Disable Gutenberg on the back end.
-				 */
-				add_filter( 'use_block_editor_for_post', '__return_false', 5 );
-
-				add_filter( 'gutenberg_can_edit_post', '__return_false', 5 );
-
-				/**
-				* Disable Gutenberg for widgets.
-				*/
-				add_filter( 'use_widgets_block_editor', '__return_false' );
-
-			}
 		}
 
 		/**
